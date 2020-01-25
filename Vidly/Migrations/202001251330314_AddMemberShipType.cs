6d57a55e8a8ac4bacb5308280500358063d11940
mohammadnazzal98@yahoo.com <mohammadnazzal98@yahoo.com>
2020-01-25 05:57:42 -0800

    end sec1

---
+++ Vidly/Migrations/202001251330314_AddMemberShipType.cs
@@ -0,0 +1,34 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddMemberShipType : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.MemberShipTypes",
+                c => new
+                    {
+                        Id = c.Byte(nullable: false),
+                        SignUp = c.Short(nullable: false),
+                        DurationInMonths = c.Byte(nullable: false),
+                        Discounts = c.Byte(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            AddColumn("dbo.Customers", "MemberShipTypeID", c => c.Byte(nullable: false));
+            CreateIndex("dbo.Customers", "MemberShipTypeID");
+            AddForeignKey("dbo.Customers", "MemberShipTypeID", "dbo.MemberShipTypes", "Id", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Customers", "MemberShipTypeID", "dbo.MemberShipTypes");
+            DropIndex("dbo.Customers", new[] { "MemberShipTypeID" });
+            DropColumn("dbo.Customers", "MemberShipTypeID");
+            DropTable("dbo.MemberShipTypes");
+        }
+    }
+}
---
+++ Vidly/Models/Customers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Customers
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsSubscribedToNewsLetter{ get; set; }
+        public MemberShipType MemberShipTypes { get; set; }
+        public byte MemberShipTypeID { get; set; }
+    }
+}
